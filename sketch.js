d218663f1587f0543a640770d3630d6f20c62da9
riteshtajrajchavhan <74711100+riteshtajrajchavhan@users.noreply.github.com>
2021-02-13 16:26:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+
+
+
+function preload(){
+  backImg=loadImage("background.png");
+  benImg=loadAnimation("ben.png","hero.png","player.png");
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+
+  back=createSprite(windowWidth/2+290*3,windowHeight/2);
+ back.addImage(backImg);
+ back.scale=2.9;
+ back.velocityX=-10;
+
+player=createSprite(windowWidth/2-400,windowHeight/2+200);
+player.addAnimation("runing",benImg);
+player.scale=1.0;
+
+wall=createSprite(windowWidth/2-400,windowHeight/2+300,300,10);
+wall.visible=false;
+
+}
+
+
+
+function draw() {
+  background("pink");  
+  drawSprites();
+
+if(back.x<0){
+  back.x=windowWidth/2+290*3;
+}
+
+
+if(keyDown("space") && player.y > windowHeight/2+200) {
+  player.velocityY = -20;
+}
+
+player.velocityY=player.velocityY+0.8;
+
+player.collide(wall);
+
+}
